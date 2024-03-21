4e0b460954105e8381c80475219c7d190ec81530
Semyon Suvorov <semen.suvoroff@yandex.ru>
2024-03-21 22:49:05 +0300

    suggestions added

---
+++ suggestions/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Suggestion
+
+
+class SuggestionAdmin(admin.ModelAdmin):
+    list_display = ["content_object", "user", "value"]
+    raw_id_fields = ["user"]
+    readonly_fields = ["content_object"]
+    search_fields = ["user__username"]
+
+
+admin.site.register(Suggestion, SuggestionAdmin)
+
